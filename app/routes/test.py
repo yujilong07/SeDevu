a3db7c407429632cd73a3c6f821644560da5c9b6
Meilong <yujilong07@gmail.com>
2026-03-19 19:54:21 +0100

    Deleted test files

--- app/routes/test.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-test_page = Blueprint('simple_page', __name__,template_folder="templates")
-
-@test_page.route('/ping')
-def test():
-    return render_template('index.html')
